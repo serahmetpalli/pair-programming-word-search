1d8eed53ee2516fe5dd3e5c748bd32a220d4a873
Serah Sirohi <serahsirohi@gmail.com>
2020-08-21 02:45:10 +0000

    added pair programming activity

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,53 @@
-const wordSearch = (letters, word) => { 
+function transpose(array) {
+    //console.log('before array: ', array); 
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < i; j++) {
+            const tmp = array[i][j];
+            array[i][j] = array[j][i];
+            array[j][i] = tmp;
+        }
+    }
+    //console.log(array);
+    return array;
+};
+
+const searchHorizontal = function(letters, word) {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const searchVertical = function(letters, word) {
+    const horizontalArray = transpose(letters);
+    if (!searchHorizontal(horizontalArray)) {
+        return false;
+    }
+    return true;
+}
+
+const wordSearch = (letters, word) => {
+    // validate
+    // word is not ''
+    if (word === '') {
+        return false
+    }
+    // letters not an array
+    if (!Array.isArray(letters)) {
+        return false;
+    }
+
+    const horizontalArray = searchHorizontal(letters, word);
+    const verticalArray = searchVertical(letters, word);
+
+    if (horizontalArray || verticalArray) {
+        return true
+    } else {
+        return false;
     }
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
